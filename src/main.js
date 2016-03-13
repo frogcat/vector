ee7bd7b16755d1fb66c0be87e005d98cbaacbb39
frogcat <frogcat@users.noreply.github.com>
2016-03-14 00:41:49 +0900

    add browserify

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+require('./leaflet-control-elevation.js');
+require('./leaflet-control-layers-search.js');
+require('./leaflet-tilelayer-geojson.js');
+var japanese = require('japanese');
+require('leaflet-hash');
+
 (function(window) {
 
   if (!location.search || location.search == "")
@@ -10,85 +16,85 @@
   var overlays = {};
 
   baseLayers["ortho"] = L.tileLayer(tmpl, {
-    id : "ort",
-    extension : "jpg"
+    id: "ort",
+    extension: "jpg"
   });
 
   baseLayers["default"] = L.tileLayer(tmpl, {
-    id : "std",
-    extension : "png"
+    id: "std",
+    extension: "png"
   });
 
   baseLayers["relief"] = L.tileLayer(tmpl, {
-    minZoom : 5,
-    maxNativeZoom : 15,
-    maxZoom : 17,
-    id : "relief",
-    extension : "png"
+    minZoom: 5,
+    maxNativeZoom: 15,
+    maxZoom: 17,
+    id: "relief",
+    extension: "png"
   });
 
   overlays["river"] = L.tileLayer.geoJson(tmpl, {
-    zoom : 16,
-    maxZoom : 20,
-    minZoom : 14,
-    id : "experimental_rvrcl",
-    extension : "geojson"
+    zoom: 16,
+    maxZoom: 20,
+    minZoom: 14,
+    id: "experimental_rvrcl",
+    extension: "geojson"
   }, function(json) {
-    return [ L.geoJson(json, {
-      className : "geojson-rvrcl"
+    return [L.geoJson(json, {
+      className: "geojson-rvrcl"
     }), L.geoJson(json, {
-      className : "geojson-rvrcl-shadow",
-      pane : "tilePane"
-    }) ];
+      className: "geojson-rvrcl-shadow",
+      pane: "tilePane"
+    })];
   });
 
   overlays["road"] = L.tileLayer.geoJson(tmpl, {
-    zoom : 16,
-    maxZoom : 20,
-    minZoom : 16,
-    id : "experimental_rdcl",
-    extension : "geojson"
+    zoom: 16,
+    maxZoom: 20,
+    minZoom: 16,
+    id: "experimental_rdcl",
+    extension: "geojson"
   }, function(json) {
-    return [ L.geoJson(json, {
-      className : "geojson-rdcl"
+    return [L.geoJson(json, {
+      className: "geojson-rdcl"
     }), L.geoJson(json, {
-      className : "geojson-rdcl-shadow",
-      pane : "tilePane"
-    }) ];
+      className: "geojson-rdcl-shadow",
+      pane: "tilePane"
+    })];
   });
 
   overlays["railway"] = L.tileLayer.geoJson(tmpl, {
-    zoom : 16,
-    maxZoom : 20,
-    minZoom : 14,
-    id : "experimental_railcl",
-    extension : "geojson"
+    zoom: 16,
+    maxZoom: 20,
+    minZoom: 14,
+    id: "experimental_railcl",
+    extension: "geojson"
   }, function(json) {
     var a = [];
     json.features.forEach(function(b) {
       if (b.properties.snglDbl == "駅部分") {
         a.push(L.geoJson(b, {
-          className : "geojson-railcl-eki"
+          className: "geojson-railcl-eki"
         }));
         a.push(L.geoJson(b, {
-          className : "geojson-railcl-eki-shadow",
-          pane : "tilePane"
+          className: "geojson-railcl-eki-shadow",
+          pane: "tilePane"
         }));
       } else if (b.properties.type == "普通鉄道（ＪＲ）") {
         a.push(L.geoJson(b, {
-          className : "geojson-railcl-jr"
+          className: "geojson-railcl-jr"
         }));
         a.push(L.geoJson(b, {
-          className : "geojson-railcl-jr-shadow",
-          pane : "tilePane"
+          className: "geojson-railcl-jr-shadow",
+          pane: "tilePane"
         }));
       } else {
         a.push(L.geoJson(b, {
-          className : "geojson-railcl"
+          className: "geojson-railcl"
         }));
         a.push(L.geoJson(b, {
-          className : "geojson-railcl-shadow",
-          pane : "tilePane"
+          className: "geojson-railcl-shadow",
+          pane: "tilePane"
         }));
       }
     });
@@ -96,18 +102,18 @@
   });
 
   overlays["anno-ja"] = L.tileLayer.geoJson(tmpl, {
-    zoom : 15,
-    maxZoom : 20,
-    minZoom : 14,
-    id : "experimental_anno",
-    extension : "geojson"
+    zoom: 15,
+    maxZoom: 20,
+    minZoom: 14,
+    id: "experimental_anno",
+    extension: "geojson"
   }, function(json) {
     var a = [];
     json.features.forEach(function(b) {
       a.push(L.marker(L.GeoJSON.coordsToLatLng(b.geometry.coordinates), {
-        icon : L.divIcon({
-          className : "geojson-anno",
-          html : b.properties.knj
+        icon: L.divIcon({
+          className: "geojson-anno",
+          html: b.properties.knj
         })
       }));
     });
@@ -115,18 +121,18 @@
   });
 
   overlays["anno-kana"] = L.tileLayer.geoJson(tmpl, {
-    zoom : 15,
-    maxZoom : 20,
-    minZoom : 14,
-    id : "experimental_anno",
-    extension : "geojson"
+    zoom: 15,
+    maxZoom: 20,
+    minZoom: 14,
+    id: "experimental_anno",
+    extension: "geojson"
   }, function(json) {
     var a = [];
     json.features.forEach(function(b) {
       a.push(L.marker(L.GeoJSON.coordsToLatLng(b.geometry.coordinates), {
-        icon : L.divIcon({
-          className : "geojson-anno",
-          html : b.properties.kana
+        icon: L.divIcon({
+          className: "geojson-anno",
+          html: b.properties.kana
         })
       }));
     });
@@ -134,18 +140,18 @@
   });
 
   overlays["anno-en"] = L.tileLayer.geoJson(tmpl, {
-    zoom : 15,
-    maxZoom : 20,
-    minZoom : 14,
-    id : "experimental_anno",
-    extension : "geojson"
+    zoom: 15,
+    maxZoom: 20,
+    minZoom: 14,
+    id: "experimental_anno",
+    extension: "geojson"
   }, function(json) {
     var a = [];
     json.features.forEach(function(b) {
       a.push(L.marker(L.GeoJSON.coordsToLatLng(b.geometry.coordinates), {
-        icon : L.divIcon({
-          className : "geojson-anno",
-          html : japanese.romanize(b.properties.kana)
+        icon: L.divIcon({
+          className: "geojson-anno",
+          html: japanese.romanize(b.properties.kana)
         })
       }));
     });
@@ -153,8 +159,8 @@
   });
 
   var map = L.map("map", L.extend({
-    maxZoom : 20,
-    minZoom : 8
+    maxZoom: 20,
+    minZoom: 8
   }, L.Hash.parseHash(location.hash)));
   map.zoomControl.setPosition("bottomright");
   map.attributionControl.addAttribution("<a href='http://maps.gsi.go.jp/development/'>地理院タイル</a>");
