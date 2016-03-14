eb99c2ab22325e990d8ee5a83001f937a7b808a2
frogcat <frogcat@users.noreply.github.com>
2016-03-14 18:46:19 +0900

    Add delay to _save

--- src/leaflet-control-layers-search.js
+++ src/leaflet-control-layers-search.js
@@ -1,9 +1,9 @@
 (function() {
 
   L.Control.Layers.Search = L.Control.Layers.extend({
-    onAdd : function(map) {
+    onAdd: function(map) {
       location.search.replace("?", "").split("/").forEach(function(key) {
-        for ( var a in this._layers) {
+        for (var a in this._layers) {
           var b = this._layers[a];
           if (b.name === key)
             b.layer.addTo(map);
@@ -12,19 +12,27 @@
       map.on("layeradd layerremove", this._save, this);
       return L.Control.Layers.prototype.onAdd.call(this, map);
     },
-    onRemove : function(map) {
+    onRemove: function(map) {
       map.off("layeradd layerremove", this._save, this);
       L.Control.Layers.prototype.onRemove.call(this, map);
     },
-    _save : function() {
-      var x = [];
-      for ( var a in this._layers) {
-        var b = this._layers[a];
-        if (this._map.hasLayer(b.layer))
-          x.push(b.name);
-      }
-      if (x.length > 0)
-        history.replaceState(null, "", "?" + x.join("/") + location.hash);
+    _save: function() {
+
+      if (this._timer)
+        clearTimeout(this._timer);
+
+      var that = this;
+      this._timer = setTimeout(function() {
+        var x = [];
+        for (var a in that._layers) {
+          var b = that._layers[a];
+          if (that._map.hasLayer(b.layer))
+            x.push(b.name);
+        }
+        if (x.length > 0)
+          history.replaceState(null, "", "?" + x.join("/") + location.hash);
+
+      }, 1000);
     }
   });
 
